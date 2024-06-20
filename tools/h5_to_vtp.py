ae958ed6b9839c328106c706879762f21836fdac
baagee <yjchoi9212@gmail.com>
2024-06-20 13:44:27 -0500

    add multiple option choice for vtk output. add deviatoric stress

--- tools/h5_to_vtp.py
+++ tools/h5_to_vtp.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
+import sys
 from pyevtk.hl import pointsToVTK
 from tqdm import tqdm
-from utils import get_h5
 import argparse
 import glob
 from collections import defaultdict
 import os
 from concurrent.futures import ProcessPoolExecutor
-
+# sys.path.append('/work2/08264/baagee/frontera/cbgeopy/utils/')
 
 def process_file(file_path):
     return pd.read_hdf(file_path, 'table')
@@ -28,13 +28,22 @@ def calculate_displacement(df):
         df['displacement_y'] ** 2 +
         df['displacement_z'] ** 2)
 
+def calculate_deviatoric_strain(df):
+    df['deviatoric_strain'] = np.sqrt(
+        (2 / 3) * ((df['strain_xx'] - df['strain_yy']) ** 2 +
+                   (df['strain_yy'] - df['strain_zz']) ** 2 +
+                   (df['strain_zz'] - df['strain_xx']) ** 2 +
+                   3 * (df['gamma_xy'] ** 2 + df['gamma_yz'] ** 2 + df['gamma_xz'] ** 2)))
+
 
 def write_vtk(df, save_path, vtk_type, t):
     x = np.ascontiguousarray(df['coord_x'])
     y = np.ascontiguousarray(df['coord_y'])
     z = np.ascontiguousarray(df['coord_z'])
+    if x.size == 0:
+        raise ValueError(f"`particles.h5` is empty at t={t}")
 
-    if vtk_type == "stresses":
+    if "stresses" in vtk_type:
         stress_xx = np.ascontiguousarray(df['stress_xx'])
         stress_yy = np.ascontiguousarray(df['stress_yy'])
         stress_zz = np.ascontiguousarray(df['stress_zz'])
@@ -54,7 +63,7 @@ def write_vtk(df, save_path, vtk_type, t):
                   "tau_xz": tau_xz,
                   "stress_magnitude": stress_magnitude})
 
-    elif vtk_type == "displacements":
+    if "displacements" in vtk_type:
         displacement_x = np.ascontiguousarray(df['displacement_x'])
         displacement_y = np.ascontiguousarray(df['displacement_y'])
         displacement_z = np.ascontiguousarray(df['displacement_z'])
@@ -68,13 +77,14 @@ def write_vtk(df, save_path, vtk_type, t):
                   "displacement_z": displacement_z,
                   "displacement": displacement})
 
-    elif vtk_type == "strains":
+    if "strains" in vtk_type:
         strain_xx = np.ascontiguousarray(df['strain_xx'])
         strain_yy = np.ascontiguousarray(df['strain_yy'])
         strain_zz = np.ascontiguousarray(df['strain_zz'])
         gamma_xy = np.ascontiguousarray(df['gamma_xy'])
         gamma_yz = np.ascontiguousarray(df['gamma_yz'])
         gamma_xz = np.ascontiguousarray(df['gamma_xz'])
+        deviatoric_strain = np.ascontiguousarray(df['deviatoric_strain'])
 
         pointsToVTK(
             f"{save_path}/particle_strains-{t}",
@@ -84,7 +94,8 @@ def write_vtk(df, save_path, vtk_type, t):
                   "strain_zz": strain_zz,
                   "gamma_xy": gamma_xy,
                   "gamma_yz": gamma_yz,
-                  "gamma_xz": gamma_xz})
+                  "gamma_xz": gamma_xz,
+                  "deviatoric_strain": deviatoric_strain})
 
     else:
         raise ValueError(f"VTK type option {vtk_type} does not exist")
@@ -100,26 +111,42 @@ def process_time_step(t, h5s_at_t, result_dir, save_path, vtk_type):
 
     df = pd.concat(dfs, ignore_index=True)
 
-    if vtk_type == "stresses":
+    if "stresses" in vtk_type:
         calculate_von_mises_stress(df)
-    elif vtk_type == "displacements":
+    if "displacements" in vtk_type:
         calculate_displacement(df)
+    if "strains" in vtk_type:
+        calculate_deviatoric_strain(df)
 
     write_vtk(df, save_path, vtk_type, t)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert hdf5 trajectories to vtk.')
-    parser.add_argument('--result_dir', default='/scratch1/08264/baagee/cbgeopy-scratch/simulations/sand_layers_geostatic/results/sand3d-resume/', help="Path to hdf5 files to consume.")
-    parser.add_argument('--vtk', default='stresses', help="Options to vtk return: ['displacements', 'stresses', 'strains']")
-    parser.add_argument('--mpi', default='32', help="Number of MPI used for mpm runtime")
-    parser.add_argument('--write_workers', default='16', help="Number of workers for vtk writing")
-    parser.add_argument('--save_path', default='/scratch1/08264/baagee/cbgeopy-scratch/simulations/sand_layers_geostatic/results/', help="Save path")
+    parser.add_argument('--result_dir',
+                        default='/work2/08264/baagee/frontera/cbgeopy/simulations/column_geostatic/results/sand3d-small_dt-N16n32/', help="Path to hdf5 files to consume.")
+    parser.add_argument('--vtk', type=str,
+                        default=['stresses', 'strains'],
+                        nargs='+',
+                        help="Options to vtk return: ['displacements', 'stresses', 'strains']")
+    parser.add_argument('--max_time',
+                        default=None,
+                        help="Max timestep to convert")
+    parser.add_argument('--mpi', type=int,
+                        default=32,
+                        help="Number of MPI used for mpm runtime")
+    parser.add_argument('--write_workers', type=int,
+                        default=8,
+                        help="Number of workers for vtk writing")
+    parser.add_argument('--save_path',
+                        default='/work2/08264/baagee/frontera/cbgeopy/debug/',
+                        help="Save path")
     args = parser.parse_args()
 
     result_dir = args.result_dir
+    max_time = args.max_time
     vtk = args.vtk
-    mpi = int(args.mpi)
+    mpi = args.mpi
     save_path = args.save_path
     write_workers = args.write_workers
 
@@ -133,8 +160,12 @@ if __name__ == "__main__":
     for files_name in files_names:
         # Extract the time component from the filename
         time = files_name.split('-')[-1].split('.')[0]
-        # Add the file to the corresponding time group
-        files_by_time[time].append(files_name)
+        if max_time is not None:
+            if int(time) <= int(max_time):
+                # Add the file to the corresponding time group
+                files_by_time[time].append(files_name)
+        else:
+            files_by_time[time].append(files_name)
 
     # Reorder the dictionary by timestep ascending order
     files_by_time = dict(sorted(files_by_time.items(), key=lambda item: int(item[0])))
