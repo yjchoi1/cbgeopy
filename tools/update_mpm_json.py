0e8aa409886d9bb0685ac09884728cfe9a72a0b7
baagee <yjchoi9212@gmail.com>
2024-10-24 23:37:45 -0500

    mpm json updator

--- tools/update_mpm_json.py
+++ tools/update_mpm_json.py
@@ -2,6 +2,7 @@ import json
 import argparse
 import re
 import glob
+import os
 
 
 def find_latest_checkpoint(result_dir):
@@ -21,9 +22,24 @@ def find_latest_checkpoint(result_dir):
     return latest_checkpoint
 
 
-class UpdateMPMjson:
-    def __init__(self, json_path):
-        self.latest_checkpoint = None
+class MPMResume:
+    def __init__(self, json_path, result_dir):
+        # Get checkpoint file names (particles h5 files)
+        self.result_dir = result_dir
+        self.file_paths = glob.glob(f"{result_dir}/particles*.h5")
+        self.file_names = [file_path.split("/")[-1] for file_path in self.file_paths]
+
+        self.file_name_info = []
+        for file_name in self.file_names:
+            pattern = re.compile(r'particles-(\d+)_(\d+)-(\d+)\.h5')
+            match = pattern.match(file_name)
+            if match:
+                mpi, n_mpis, timestep = match.groups()
+                self.file_name_info.append({
+                    'filename': file_name,
+                    'mpi': f"{mpi}_{n_mpis}",
+                    'timestep': timestep
+                })
 
         # Open and load the JSON file as a dictionary
         with open(json_path, 'r') as file:
@@ -31,7 +47,26 @@ class UpdateMPMjson:
 
         self.json_path = json_path
 
-    def to_latest_checkpoint(self, result_dir: str, fixed_checkpoint: int = None):
+        # Get final simulation steps
+        self.nsteps = self.mpm_json["analysis"]["nsteps"]
+
+    def process_checkpoint_filename(self):
+        final_checkpoint_digits = len(str(self.nsteps))
+
+        for i, info in enumerate(self.file_name_info):
+            padded_timestep = info["timestep"].zfill(final_checkpoint_digits)
+            new_filename = f'particles-{info["mpi"]}-{padded_timestep}.h5'
+            new_path = os.path.join(self.result_dir, new_filename)
+            if self.file_paths[i] != new_path:
+                os.rename(self.file_paths[i], new_path)
+                print(f'Renamed: {self.file_paths[i]} -> {new_filename}')
+            else:
+                print(f'Skipped: {self.file_paths[i]} (has the same digits)')
+
+    def to_latest_checkpoint(
+            self, result_dir: str,
+            fixed_checkpoint: int = None,
+    ):
         if fixed_checkpoint is None:
             self.latest_checkpoint = find_latest_checkpoint(result_dir)
         else:
@@ -73,20 +108,33 @@ if __name__ == "__main__":
     parser.add_argument(
         '--update_option',
         choices=['to_latest_checkpoint', 'to_first_checkpoint', 'remove_particle_constraints'],
-        default='to_latest_checkpoint',
+        default=None,
         help="The options specifying what to update")
 
     # Parse the arguments
     args = parser.parse_args()
 
-    update_mpm_json = UpdateMPMjson(json_path=args.json_path)
-    if args.update_option == 'to_latest_checkpoint':
-        update_mpm_json.to_latest_checkpoint(result_dir=args.result_dir)
-    elif args.update_option == 'to_first_checkpoint':
-        update_mpm_json.to_latest_checkpoint(result_dir=args.result_dir, fixed_checkpoint=0)
-    elif args.upate_option == 'remove_particle_constraints':
-        update_mpm_json.remove_particle_constraints()
-    else:
-        raise ValueError(f"Not a valid option for {args.update_option}")
+    json_path = args.json_path
+    result_dir = args.result_dir
+    update_option = args.update_option
 
-    update_mpm_json.save_updated_json()
+    # json_path = '/work2/08264/baagee/frontera/cbgeopy/examples/sand_layers-2d-random/sim-1001/mpm-resume.json'
+    # result_dir = '/work2/08264/baagee/frontera/cbgeopy/examples/sand_layers-2d-random/sim-1001/results/sand2d/'
+    # update_option = 'to_latest_checkpoint'
+
+    mpm_resume = MPMResume(json_path=json_path, result_dir=result_dir)
+
+    if update_option is not None:
+        # Update particle h5 checkpoint timestep to proper digit
+        mpm_resume.process_checkpoint_filename()
+
+        if update_option == 'to_latest_checkpoint':
+            mpm_resume.to_latest_checkpoint(result_dir=result_dir)
+        elif update_option == 'to_first_checkpoint':
+            mpm_resume.to_latest_checkpoint(result_dir=result_dir, fixed_checkpoint=0)
+        elif update_option == 'remove_particle_constraints':
+            mpm_resume.remove_particle_constraints()
+        else:
+            raise ValueError(f"Not a valid option for {update_option}")
+
+    mpm_resume.save_updated_json()
